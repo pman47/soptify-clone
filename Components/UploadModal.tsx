3ce739d8d77b794ff468a1bc9ddf5393025a03b2
Manish Prajapati <manish.prajapati@queensenglish.co>
2023-08-20 17:09:34 +0530

    Created form and reusing input component to show different input fields

--- Components/UploadModal.tsx
+++ Components/UploadModal.tsx
@@ -1,25 +1,79 @@
 "use client";
+import { useForm, FieldValues } from "react-hook-form";
+
 import useUploadModal from "@/hooks/useUploadModal";
 import Modal from "./Modal";
+import { SubmitHandler } from "react-hook-form/dist/types";
+import { useState } from "react";
+import Input from "./Input";
 
 const UploadModal = () => {
   const uploadModal = useUploadModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      author: "",
+      title: "",
+      song: null,
+      image: null,
+    },
+  });
 
   const onChange = (open: boolean) => {
     if (!open) {
-      //   Reset the form
+      reset();
       uploadModal.onClose();
     }
   };
 
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    // Upload to Supabase
+  };
+
   return (
     <Modal
-      title="Upload Modal Title"
-      description="Upload Modal Description"
+      title="Add a song"
+      description="Upload an mp3 file"
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      Upload Content
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
+        <Input
+          id="title"
+          type="text"
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder="Song Title"
+        />
+        <Input
+          id="author"
+          type="text"
+          disabled={isLoading}
+          {...register("author", { required: true })}
+          placeholder="Song Author"
+        />
+        <div>
+          <div className="pb-1">Select a song file</div>
+          <Input
+            id="song"
+            type="file"
+            disabled={isLoading}
+            accept="audio/*"
+            {...register("song", { required: true })}
+          />
+        </div>
+        <div>
+          <div className="pb-1">Select an image</div>
+          <Input
+            id="song"
+            type="file"
+            disabled={isLoading}
+            accept="image/*"
+            {...register("image", { required: true })}
+          />
+        </div>
+      </form>
     </Modal>
   );
 };
