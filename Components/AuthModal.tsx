1ba7099b40cd44796ed07c47821aa729f3c5eaae
Manish Prajapati <manish.prajapati@queensenglish.co>
2023-08-20 15:50:02 +0530

    Imported auth modal and options from supabase auth ui

--- Components/AuthModal.tsx
+++ Components/AuthModal.tsx
@@ -1,18 +1,55 @@
 "use client";
 import { FC } from "react";
+import {
+  useSupabaseClient,
+  useSessionContext,
+} from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+
 import Modal from "./Modal";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import useAuthModal from "@/hooks/useAuthModal";
 
 interface AuthModalProps {}
 
 const AuthModal: FC<AuthModalProps> = ({}) => {
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { session } = useSessionContext();
+
+  const { onClose, isOpen } = useAuthModal();
+
+  const onChange = (open: boolean) => {
+    if (open) {
+      onClose();
+    }
+  };
+
   return (
     <Modal
       title="Welcome back"
       description="Login to your account"
-      isOpen
-      onChange={() => {}}
+      isOpen={isOpen}
+      onChange={onChange}
     >
-      Auth Modal children!
+      <Auth
+        theme="dark"
+        magicLink
+        providers={["github"]}
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "#404040",
+                brandAccent: "#22c55e",
+              },
+            },
+          },
+        }}
+      />
     </Modal>
   );
 };
