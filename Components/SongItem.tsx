a4b9979c2397d8836fb5e8efb53087bc215c82ab
Manish Prajapati <manishprajapatim416@gmail.com>
2023-08-23 21:17:22 +0530

    Created PlayButton component to show button on song hover event

--- Components/SongItem.tsx
+++ Components/SongItem.tsx
@@ -2,6 +2,7 @@ import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
 import Image from "next/image";
 import { FC } from "react";
+import PlayButton from "./PlayButton";
 
 interface SongItemProps {
   onClick: (id: string) => void;
@@ -24,11 +25,14 @@ const SongItem: FC<SongItemProps> = ({ data, onClick }) => {
           alt="Image"
         />
       </div>
-      <div className="flex flex-col items-start w-full p-4 gap-y-1">
+      <div className="flex flex-col items-start w-full pt-4 gap-y-1">
         <p className="font-semibold truncate w-full">{data.title}</p>
         <p className="text-neutral-400 text-sm pb-4 w-full truncate">
           By {data.author}
         </p>
+      </div>
+      <div className="absolute bottom-24 right-5">
+        <PlayButton />
       </div>
     </div>
   );
